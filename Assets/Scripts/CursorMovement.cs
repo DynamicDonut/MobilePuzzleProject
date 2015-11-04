1d534085545bfb379c758b37c67f30cdff84d39d
DynamicDonut <bwilldonut@gmail.com>
2015-11-03 19:46:43 -0500

    Keyboard Movement
    Very basic

---
+++ Assets/Scripts/CursorMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CursorMovement : MonoBehaviour {
+	int MovementType = 0; //0 = KB, 1 = Mouse, 2 = Gamepad, 3 = Touch/Mobile
+	GameMangerScript MainScript;
+
+	// Use this for initialization
+	void Start () {
+		MainScript = GameObject.Find ("GameManager").GetComponent<GameMangerScript> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (MovementType == 0) {
+			if (Input.GetKeyUp(KeyCode.LeftArrow)){
+				transform.position = transform.position + Vector3.left * MainScript.tileW;
+			} else if (Input.GetKeyUp(KeyCode.RightArrow)){
+				transform.position = transform.position + Vector3.right * MainScript.tileW;
+			} else if (Input.GetKeyUp(KeyCode.UpArrow)){
+				transform.position = transform.position + Vector3.up * MainScript.tileH;
+			} else if (Input.GetKeyUp(KeyCode.DownArrow)){
+				transform.position = transform.position + Vector3.down * MainScript.tileH;
+			}
+
+			if (Input.GetKeyUp(KeyCode.Space)){
+
+			}
+		} else if (MovementType == 1) {
+		} else if (MovementType == 2) {
+		} else if (MovementType == 3) {
+		}
+	}
+}
--- Assets/Scripts/GameMangerScript.cs
+++ Assets/Scripts/GameMangerScript.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class GameMangerScript : MonoBehaviour {
 	public GameObject PuzzBlock;
+	public GameObject PuzzCursor;
+
+	public float tileW; public float tileH;
 	Vector3 StartPos = Vector3.zero;
 	int numCol = 6;
 	int numRow = 5;
@@ -11,22 +14,28 @@ public class GameMangerScript : MonoBehaviour {
 	Vector3 cameraPos;
 
 	void Start(){
-		PuzzFieldW = (numCol - 1) * 16f + 8f;
+		tileW = PuzzBlock.GetComponent<Renderer> ().bounds.size.x;
+		tileH = PuzzBlock.GetComponent<Renderer> ().bounds.size.y;
+
+		PuzzFieldW = (numCol - 1) * tileW + 8f;
 		cameraPos = new Vector3 (PuzzFieldW / 2, 66f, -10);
 		for (int i = 0; i < numRow; i++){
 			for (int j = 0; j < numCol; j++){ 
 				GameObject newBlock = Instantiate(PuzzBlock);
 				newBlock.transform.position = StartPos;
 				newBlock.transform.name = "PuzzBlock_"+i+"-"+j;
-				StartPos.x += 16f;
+				StartPos.x += tileW;
 			}
 			StartPos.x = 0.0f;
-			StartPos.y += 16f;
+			StartPos.y += tileH;
 		}
+
+		GameObject GameCursor = (GameObject)Instantiate(PuzzCursor, (Vector2.right * tileW/2), transform.rotation);
+
 		Camera.main.transform.position = cameraPos;
 	}
 
 	void Update () {
-		
+		//Debug.Log ();
 	}
 }
